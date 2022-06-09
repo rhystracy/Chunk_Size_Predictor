b0b6283e9f9ec2eef6c7798955c1202c4ea984f9
Rhys <rhystracy@ucsb.edu>
2022-06-08 23:41:03 -0700

    rt - cleaned up final code and sample data done

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,46 +6,10 @@ import glob, os, subprocess, shlex, shutil
 def get_csvs():
     return [file for file in glob.glob("./CSVs/*.csv")]
 
-def get_csvs_duplicates():
-    return [file for file in glob.glob("./CSVs_duplicates/*.csv")]
+def get_csvs_testing():
+    return [file for file in glob.glob("./CSVs_testing/*.csv")]
 
-def get_data_one(csv, num_prev_chunks, num_prev_periods, period):
-    data_chunks = list()
-    targets_chunks = list()
-    data_burstrate = list()
-    targets_burstrate = list()
-    chunks = get_chunk_sizes(csv, num_prev_chunks)
-    try:
-        chunks = get_chunk_sizes(csv, num_prev_chunks)
-        for i in range(chunks.shape[0]-1):
-            targets_chunks.append(chunks[i+1][num_prev_chunks-1])
-            data_chunks.append(chunks[i])
-    except pd.errors.ParserError:
-        print("EOF error:", csv)
-    except IndexError:
-        print("Index Error:", csv, "doesn't have at least", num_prev_chunks+1, "chunks")
-    
-    try:
-        burstrates = get_burstrates(csv, num_prev_periods, period)
-        for i in range(burstrates.shape[0]-1):
-            targets_burstrate.append(burstrates[i+1][num_prev_periods-1])
-            data_burstrate.append(burstrates[i])
-    except pd.errors.ParserError:
-        print("EOF error:", csv)
-    except IndexError:
-        print("Index Error:", csv, "doesn't have at least", num_prev_periods+1, "periods")
-    
-    data_chunks = np.asarray(data_chunks)
-    targets_chunks = np.asarray(targets_chunks)
-
-    data_burstrate = np.asarray(data_burstrate)
-    targets_burstrate = np.asarray(targets_burstrate)
-
-    return data_chunks, targets_chunks, data_burstrate, targets_burstrate
-
-def make_dataset(num_prev_chunks, num_prev_periods, period):
-    data_chunks = list()
-    targets_chunks = list()
+def make_dataset(num_prev_periods, period):
     data_burstrate = list()
     targets_burstrate = list()
     csvs = get_csvs()
@@ -58,18 +22,6 @@ def make_dataset(num_prev_chunks, num_prev_periods, period):
             cur_percent+=10
         a+=1
 
-        '''try:
-            chunks = get_chunk_sizes(csv, num_prev_chunks)
-            for i in range(chunks.shape[0]-1):
-                targets_chunks.append(chunks[i+1][num_prev_chunks-1])
-                data_chunks.append(chunks[i])
-        except pd.errors.ParserError:
-            print("EOF error:", csv)
-        except IndexError:
-            print("Index Error:", csv, "doesn't have at least", num_prev_chunks+1, "chunks")
-        except KeyError:
-            print("Key Error:", csv, "no quic packets, can't get client ip")
-        '''
         try:
             burstrates = get_burstrates(csv, num_prev_periods, period)
             for i in range(burstrates.shape[0]-1):
@@ -82,22 +34,17 @@ def make_dataset(num_prev_chunks, num_prev_periods, period):
         except KeyError:
             print("Key Error:", csv, "no quic packets, can't get client ip")
     
-    data_chunks = np.asarray(data_chunks)
-    targets_chunks = np.asarray(targets_chunks)
-
     data_burstrate = np.asarray(data_burstrate)
     targets_burstrate = np.asarray(targets_burstrate)
 
     print("100 percent done")
 
-    return data_chunks, targets_chunks, data_burstrate, targets_burstrate
+    return data_burstrate, targets_burstrate
 
-def make_dataset_duplicates(num_prev_chunks, num_prev_periods, period):
-    data_chunks = list()
-    targets_chunks = list()
+def make_dataset_testing(num_prev_periods, period):
     data_burstrate = list()
     targets_burstrate = list()
-    csvs = get_csvs_duplicates()
+    csvs = get_csvs_testing()
     num_csvs = len(csvs)
     a=0
     cur_percent = 0
@@ -107,18 +54,6 @@ def make_dataset_duplicates(num_prev_chunks, num_prev_periods, period):
             cur_percent+=10
         a+=1
 
-        '''try:
-            chunks = get_chunk_sizes(csv, num_prev_chunks)
-            for i in range(chunks.shape[0]-1):
-                targets_chunks.append(chunks[i+1][num_prev_chunks-1])
-                data_chunks.append(chunks[i])
-        except pd.errors.ParserError:
-            print("EOF error:", csv)
-        except IndexError:
-            print("Index Error:", csv, "doesn't have at least", num_prev_chunks+1, "chunks")
-        except KeyError:
-            print("Key Error:", csv, "no quic packets, can't get client ip")
-        '''
         try:
             burstrates = get_burstrates(csv, num_prev_periods, period)
             for i in range(burstrates.shape[0]-1):
@@ -130,131 +65,19 @@ def make_dataset_duplicates(num_prev_chunks, num_prev_periods, period):
             print("Index Error:", csv, "doesn't have at least", num_prev_periods+1, "periods")
         except KeyError:
             print("Key Error:", csv, "no quic packets, can't get client ip")
-    
-    data_chunks = np.asarray(data_chunks)
-    targets_chunks = np.asarray(targets_chunks)
 
     data_burstrate = np.asarray(data_burstrate)
     targets_burstrate = np.asarray(targets_burstrate)
 
     print("100 percent done")
 
-    return data_chunks, targets_chunks, data_burstrate, targets_burstrate
-
-def get_chunk_sizes(file, num_prev_chunks):
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    #df = pd.read_csv('data/example_pcap.csv')
-    df = pd.read_csv(file)#, dtype={'frame.number':'int','frame.time':'object', 'frame.time_relative':'float',
-                    #'frame.time_delta':'float', 'ip.src':'object', 'ip.dst':'object', 'ip.proto':'int', 'frame.len':'int',
-                    #'frame.protocols':'object', 'data.len':'int', 'quic':'object'})
-
-    #dataset = pd.get_dummies(dataset)
-    #dataset = dataset.replace(np.nan, 0)
-    #dataset = dataset.to_numpy()
-
-    df_quic = df[df["quic"] == "quic"] #only need quic packets bc these are the only packets for the video
-
-    df_quic = df_quic.reset_index(drop=True) #reset df indices after dropping rows
-
-    client = df_quic['ip.src'][0] #client sends first request to server
-
-    df = df[df['ip.dst'] == client] #only look at packets incoming to the client
-
-    df = df.reset_index(drop=True) #reset df indices after dropping rows
-
-
-    #go through and calculate chunk sizes
-
-    chunk_size_data = []
-    prev_x = []
-
-    last = 0 #0=udp last, 1=quic last
-    chunk_size_udp = 0
-    chunk_size_quic = 0
-    prev_start_time_udp = 0
-    prev_start_time_quic = 0
-    for i in range(len(df)):
-        if(not("eth:ethertype:ip:udp:data" == df['frame.protocols'][i] or "eth:ethertype:ip:udp:quic" == df['frame.protocols'][i])):
-            continue
-        else:
-            if(prev_start_time_udp == 0 and "eth:ethertype:ip:udp:data" == df['frame.protocols'][i]):
-                prev_start_time_udp = df['frame.time_relative'][i]
-            if(prev_start_time_quic == 0 and "eth:ethertype:ip:udp:quic" == df['frame.protocols'][i]):
-                prev_start_time_quic = df['frame.time_relative'][i]
-            if("eth:ethertype:ip:udp:data" == df['frame.protocols'][i] and df['frame.time_relative'][i]>prev_start_time_udp + 0.1): #more than 0.1s since last packet, means new chunk
-                if(len(prev_x) == num_prev_chunks):
-                    chunk_size_data.append(prev_x[:])
-                    prev_x.pop(0)
-                    prev_x.append(chunk_size_udp)
-                else:
-                    prev_x.append(chunk_size_udp)
-                chunk_size_udp = 0
-                prev_start_time_udp = df['frame.time_relative'][i]
-                last = 0
-            if("eth:ethertype:ip:udp:quic" == df['frame.protocols'][i] and df['frame.time_relative'][i]>prev_start_time_quic + 0.1): #more than 0.1s since last packet, means new chunk
-                if(len(prev_x) == num_prev_chunks):
-                    chunk_size_data.append(prev_x[:])
-                    prev_x.pop(0)
-                    prev_x.append(chunk_size_quic)
-                else:
-                    prev_x.append(chunk_size_quic)
-                chunk_size_quic = 0
-                prev_start_time_quic = df['frame.time_relative'][i]
-                last = 1
-            if("eth:ethertype:ip:udp:data" == df['frame.protocols'][i]):
-                chunk_size_udp += int(df['frame.len'][i])
-                prev_start_time_udp = df['frame.time_relative'][i]
-            if("eth:ethertype:ip:udp:quic" == df['frame.protocols'][i]):
-                chunk_size_quic += int(df['frame.len'][i])
-                prev_start_time_quic = df['frame.time_relative'][i]
-
-    if(last == 0):
-        if(len(prev_x) == num_prev_chunks):
-            chunk_size_data.append(prev_x[:])
-            prev_x.pop(0)
-            prev_x.append(chunk_size_quic)
-        else:
-            prev_x.append(chunk_size_quic)
-        if(len(prev_x) == num_prev_chunks):
-            chunk_size_data.append(prev_x[:])
-            prev_x.pop(0)
-            prev_x.append(chunk_size_udp)
-        else:
-            prev_x.append(chunk_size_udp)
-        if(len(prev_x) == num_prev_chunks):
-            chunk_size_data.append(prev_x[:])
-    else:
-        if(len(prev_x) == num_prev_chunks):
-            chunk_size_data.append(prev_x[:])
-            prev_x.pop(0)
-            prev_x.append(chunk_size_udp)
-        else:
-            prev_x.append(chunk_size_udp)
-        if(len(prev_x) == num_prev_chunks):
-            chunk_size_data.append(prev_x[:])
-            prev_x.pop(0)
-            prev_x.append(chunk_size_quic)
-        else:
-            prev_x.append(chunk_size_quic)
-        if(len(prev_x) == num_prev_chunks):
-            chunk_size_data.append(prev_x[:])
-
-    chunk_size_data=np.asarray(chunk_size_data)
-
-    #print(chunk_size_data)
-    #print(chunk_size_data.shape)
-
-    return chunk_size_data
+    return data_burstrate, targets_burstrate
 
 
 def get_burstrates(file, num_prev_periods, period):
     timestr = time.strftime("%Y%m%d-%H%M%S")
     #df = pd.read_csv('data/example_pcap.csv')
     df = pd.read_csv(file)
-
-    #dataset = pd.get_dummies(dataset)
-    #dataset = dataset.replace(np.nan, 0)
-    #dataset = dataset.to_numpy()
 
     df_quic = df[df["quic"] == "quic"] #only need quic packets bc these are the only packets for the video
 
@@ -305,8 +128,6 @@ def get_burstrates(file, num_prev_periods, period):
 
 
 if __name__ == '__main__':
-    data_chunks, targets_chunks, data_burstrate, targets_burstrate = make_dataset(num_prev_chunks=5, num_prev_periods=5, period=5)
-    print(data_chunks.shape)
-    print(targets_chunks.shape)
+    data_burstrate, targets_burstrate = make_dataset(num_prev_chunks=5, num_prev_periods=5, period=5)
     print(data_burstrate.shape)
     print(targets_burstrate.shape)
--- train.py
+++ train.py
@@ -1,17 +1,16 @@
 import numpy as np
 import tensorflow as tf
 from model import TRANS_Model
-from feature_engineering import make_dataset, make_dataset_duplicates, get_data_one
+from feature_engineering import make_dataset, make_dataset_testing
 
 def train_and_evaluate():
     #vars
-    num_prev_chunks = 5
     num_prev_periods = 5
     period = 15
 
     #read csvs and make datasets
     print("Creating Dataset:")
-    data_chunks, targets_chunks, data_burstrate, targets_burstrate = make_dataset(num_prev_chunks=num_prev_chunks, num_prev_periods=num_prev_periods, period=period)
+    data_burstrate, targets_burstrate = make_dataset(num_prev_periods=num_prev_periods, period=period)
     '''order = np.arange(data_burstrate.shape[0])
     np.random.shuffle(order)
     print(order)
@@ -21,47 +20,7 @@ def train_and_evaluate():
     #targets_burstrate = targets_burstrate/1000 #convert burstrates to kb/s
     print("")
 
-    ####Chunks:
-
-    '''#set up training data
-    print("Setting up chunk data")
-    print("")
-    split_point_chunks = int(0.9*data_chunks.shape[0])
-
-    train_x_chunks = data_chunks[0:split_point_chunks]
-    train_y_chunks = targets_chunks[0:split_point_chunks]
-
-    train_x_chunks = train_x_chunks.reshape((train_x_chunks.shape[0], train_x_chunks.shape[1], 1))
-    train_y_chunks = train_y_chunks.reshape(-1,1)
-
-    #initialize model
-    print("Training chunk model")
-    print("")
-    trans_model_chunks = TRANS_Model(train_x_chunks.shape[1:])
-
-    #train model
-    trans_model_chunks.train(train_x_chunks, train_y_chunks)
-
-    #set up test data
-    test_x_chunks = data_chunks[split_point_chunks:]
-    test_y_chunks = targets_chunks[split_point_chunks:]
-
-    test_x_chunks = test_x_chunks.reshape((test_x_chunks.shape[0], test_x_chunks.shape[1], 1))
-    test_y_chunks = test_y_chunks.reshape(-1,1)
-
-    #evaluate
-    eval_chunks = trans_model_chunks.evaluate(test_x_chunks, test_y_chunks)
-    avg_chunks = np.average(targets_chunks[split_point_chunks:])
     
-    #print(eval)
-    print("")
-    print("Chunk Sizes:")
-    print("Mean Squared Error (bytes): ", eval_chunks[1])
-    print("Mean Absolute Error (bytes): ", eval_chunks[0])
-    print("Average Testing Chunk Size (bytes)", avg_chunks)
-    print("")
-    '''
-
     ####burst rates:
     
     #set up training data
@@ -90,90 +49,44 @@ def train_and_evaluate():
     test_x_burstrate = test_x_burstrate.reshape((test_x_burstrate.shape[0], test_x_burstrate.shape[1], 1))
     test_y_burstrate = test_y_burstrate.reshape(-1,1)
 
-    #evaluate
-    '''
-    eval_burstrate = trans_model_burstrate.evaluate(test_x_burstrate, test_y_burstrate)
-    avg_burstrate = np.average(targets_burstrate[split_point_burstrate:])
-    preds = trans_model_burstrate.predict(test_x_burstrate)
-    avg_percent_errors = tf.keras.metrics.mean_absolute_percentage_error(test_y_burstrate, preds)
-    pct_under_100 = 0
-    pct_over_100 = 0
-    avg_under_100 = 0
-    for i in range(avg_percent_errors.shape[0]):
-        if(avg_percent_errors[i]<100):
-            pct_under_100 += 1
-            avg_under_100 += avg_percent_errors[i]
-        else:
-            pct_over_100 += 1
-    
-    avg_under_100 = float(avg_under_100)/float(pct_under_100)
-    pct_over_100 = float(pct_over_100)/float(avg_percent_errors.shape[0])
-    pct_under_100 = float(pct_under_100)/float(avg_percent_errors.shape[0])
-    
-    #print(eval)
-    print("")
-    print("Burst rates:")
-    print("Mean Squared Error (bytes): ", eval_burstrate[1])
-    print("Mean Absolute Error (bytes): ", eval_burstrate[0])
-    print("Mean Absolute Percent Error (when under 100%): ", avg_under_100)
-    print("Percent of predictions with <100 percent error: ", pct_under_100)
-    print("Percent of predictions with >100 percent error: ", pct_over_100)
-    print("Average Testing Burst rate (bytes)", avg_burstrate)
-    print("")
-    '''
 
-    ####Testing on duplicate streams:
+    ####Testing:
 
-    #get duplicates data
+    #get testing data
     print("Getting Testing Data:")
-    data_chunks_duplicates, targets_chunks_duplicates, data_burstrate_duplicates, targets_burstrate_duplicates = make_dataset_duplicates(num_prev_chunks=num_prev_chunks, num_prev_periods=num_prev_periods, period=period)
+    data_burstrate_testing, targets_burstrate_testing = make_dataset_testing(num_prev_periods=num_prev_periods, period=period)
     print("")
-
-    '''
-    #evaluate duplicate chunks
-    data_chunks_duplicates = data_chunks_duplicates.reshape((data_chunks_duplicates.shape[0], data_chunks_duplicates.shape[1], 1))
-    targets_chunks_duplicates = targets_chunks_duplicates.reshape(-1,1)
-    eval_chunks_duplicates = trans_model_chunks.evaluate(data_chunks_duplicates, targets_chunks_duplicates)
-    avg_chunks_duplicates = np.average(targets_chunks)
-    
-    #print(eval_chunks)
-    print("Chunk Sizes Duplicate:")
-    print("Mean Squared Error (bytes): ", eval_chunks_duplicates[1])
-    print("Mean Absolute Error (bytes): ", eval_chunks_duplicates[0])
-    print("Average Chunk Size (bytes)", avg_chunks_duplicates)
-    print("")
-    '''
 
     
     #evaluate duplicate burst rates
-    data_burstrate_duplicates = data_burstrate_duplicates.reshape((data_burstrate_duplicates.shape[0], data_burstrate_duplicates.shape[1], 1))
-    targets_burstrate_duplicates = targets_burstrate_duplicates.reshape(-1,1)
-    eval_burstrate_duplicates = trans_model_burstrate.evaluate(data_burstrate_duplicates, targets_burstrate_duplicates)
-    avg_burstrate_duplicates = np.average(targets_burstrate_duplicates)
-    preds = trans_model_burstrate.predict(data_burstrate_duplicates)
-    avg_percent_errors_duplicates = tf.keras.metrics.mean_absolute_percentage_error(targets_burstrate_duplicates, preds)
+    data_burstrate_testing = data_burstrate_testing.reshape((data_burstrate_testing.shape[0], data_burstrate_testing.shape[1], 1))
+    targets_burstrate_testing = targets_burstrate_testing.reshape(-1,1)
+    eval_burstrate_testing = trans_model_burstrate.evaluate(data_burstrate_testing, targets_burstrate_testing)
+    avg_burstrate_testing = np.average(targets_burstrate_testing)
+    preds = trans_model_burstrate.predict(data_burstrate_testing)
+    avg_percent_errors_testing = tf.keras.metrics.mean_absolute_percentage_error(targets_burstrate_testing, preds)
     pct_under_100 = 0
     pct_over_100 = 0
     avg_under_100 = 0
-    for i in range(avg_percent_errors_duplicates.shape[0]):
-        if(avg_percent_errors_duplicates[i]<100):
+    for i in range(avg_percent_errors_testing.shape[0]):
+        if(avg_percent_errors_testing[i]<100):
             pct_under_100 += 1
-            avg_under_100 += avg_percent_errors_duplicates[i]
+            avg_under_100 += avg_percent_errors_testing[i]
         else:
             pct_over_100 += 1
     
     avg_under_100 = float(avg_under_100)/float(pct_under_100)
-    pct_over_100 = float(pct_over_100)/float(avg_percent_errors_duplicates.shape[0])
-    pct_under_100 = float(pct_under_100)/float(avg_percent_errors_duplicates.shape[0])
+    pct_over_100 = float(pct_over_100)/float(avg_percent_errors_testing.shape[0])
+    pct_under_100 = float(pct_under_100)/float(avg_percent_errors_testing.shape[0])
     
     #print(eval)
-    print("Burst rates duplicates:")
-    print("Mean Squared Error (bytes): ", eval_burstrate_duplicates[1])
-    print("Mean Absolute Error (bytes): ", eval_burstrate_duplicates[0])
+    print("Burst Rates Testing:")
+    print("Mean Squared Error (bytes): ", eval_burstrate_testing[1])
+    print("Mean Absolute Error (bytes): ", eval_burstrate_testing[0])
     print("Mean Absolute Percent Error (when under 100%): ", avg_under_100)
     print("Percent of predictions with <100 percent error: ", pct_under_100)
     print("Percent of predictions with >100 percent error: ", pct_over_100)
-    print("Average Burst rate (bytes)", avg_burstrate_duplicates)
+    print("Average Burst rate (bytes)", avg_burstrate_testing)
     
 
     #outs = trans_model.predict(test_x)
